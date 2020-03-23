7a9586ef31a0b31c5ec9724e92eb2d6da9a16e48
Shariful Alam <shariful.alam85@gmail.com>
2020-03-23 20:36:32 +0600

    user spec updated

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    email {Faker::Internet.email}
+    name {'Test Name'}
+    phone {'000111'}
+    target_amount 10000
+    bonus_percentage 10
+    password {'11111'}
+    password_confirmation {'11111'}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = FactoryBot.build(:user)
+  end
+  describe "User" do
+    it "should be created with valid attribute" do
+      count = 0
+      @user.save
+      expect(@user.valid?).to eq(true)
+      expect(count+1).to eq(User.count)
+    end
+  end
 end
