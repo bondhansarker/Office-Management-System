89a44b35a344400dc78729de31c571c3179aeddd
bondhansarker <bondhansarker100@gmail.com>
2020-03-25 05:30:30 +0600

    context added

--- spec/models/budget_spec.rb
+++ spec/models/budget_spec.rb
@@ -8,30 +8,32 @@ RSpec.describe Budget, type: :model do
   end
 
   describe "Budget" do
-    it "should be created with valid attribute" do
-      count = 0
-      @budget.save
-      expect(@budget.valid?).to be(true)
-      expect(count+1).to eq(Budget.count)
+    context "create" do
+      it "should be created with valid attribute" do
+        count = 0
+        @budget.save
+        expect(@budget.valid?).to be(true)
+        expect(count+1).to eq(Budget.count)
+      end
     end
-  end
 
-  describe "Budget" do
-    it "should add budget with existing budget" do
-      amount = @budget.amount
-      @budget.save
-      @budget.update({add: 2000})
-      expect(@budget.amount).to eq(amount+2000)
+    context "#add_budget_method" do
+      it "should add budget with existing budget while update" do
+        amount = @budget.amount
+        @budget.save
+        @budget.update({add: 2000})
+        expect(@budget.amount).to eq(amount+2000)
+      end
     end
-  end
 
-  describe "Budget" do
-    it "should search budgets with year and month" do
-      @budget.save
-      expect(Budget.search_with(2020,1).first).to eq(@budget)
-      expect(Budget.search_with(2020,1).sum(:amount)).to eq(@budget.amount)
-      expect(Budget.search_with(2020,1).sum(:expense)).to eq(@budget.expense)
+    context "#search_with_scope" do
+      it "should search budgets with year and month and return a collection" do
+        @budget.save
+        expect(Budget.search_with(2020, 1).first).to eq(@budget)
+        expect(Budget.search_with(2020, 1).sum(:amount)).to eq(@budget.amount)
+        expect(Budget.search_with(2020, 1).sum(:expense)).to eq(@budget.expense)
+      end
     end
-  end
 
+  end
 end
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -10,70 +10,70 @@ RSpec.describe Expense, type: :model do
   end
 
   describe "Expense" do
-    it "should be created with valid attribute" do
-      count = 0
-      @budget.save
-      @expense.save
-      expect(@expense.valid?).to eq(true)
-      expect(count+1).to eq(Expense.count)
+    context "#create" do
+      it "should be created with valid attribute" do
+        count = 0
+        @budget.save
+        @expense.save
+        expect(@expense.valid?).to eq(true)
+        expect(count+1).to eq(Expense.count)
+      end
     end
-  end
 
-  describe "Expense" do
-    it "should be approved automatically submitted by admin" do
-      @expense = FactoryBot.build(:expense, user: @user, category: @category)
-      @budget.save
-      @expense.save
-      expect(@expense.cost).to eq(@expense.budget.expense)
-      expect(Expense.approved.count).to eq(1)
+    context "#admin_approval_method" do
+      it "should be approved automatically submitted by admin" do
+        @expense = FactoryBot.build(:expense, user: @user, category: @category)
+        @budget.save
+        @expense.save
+        expect(@expense.cost).to eq(@expense.budget.expense)
+        expect(Expense.approved.count).to eq(1)
+      end
     end
-  end
 
-  describe "Expense" do
-    it "should decrease budget amount by approving an expense" do
-      @expense.user.role = "Employee"
-      @budget.save
-      @expense.save
-      @expense.approved!
-      @expense.update_budget
-      expect(@expense.cost).to eq(@expense.budget.expense)
+    context "#approve expense" do
+      it "should decrease budget amount by approving" do
+        @expense.user.role = "Employee"
+        @budget.save
+        @expense.save
+        @expense.approved!
+        @expense.update_budget
+        expect(@expense.cost).to eq(@expense.budget.expense)
+      end
     end
-  end
 
-  describe "Expense" do
-    it "should increase budget amount by making undo of an expense" do
-      @expense.user.role = "Employee"
-      @budget.save
-      @expense.save
-      @expense.approved!
-      @expense.update_budget
-      @expense.pending!
-      @expense.update_budget
-      expect(@expense.budget.expense).to eq(0)
+    context "#undo expense" do
+      it "should increase budget amount by making undo of an expense" do
+        @expense.user.role = "Employee"
+        @budget.save
+        @expense.save
+        @expense.approved!
+        @expense.update_budget
+        @expense.pending!
+        @expense.update_budget
+        expect(@expense.budget.expense).to eq(0)
+      end
     end
-  end
 
-  describe "Expense" do
-    it "should be created with valid budget year" do
-      count = 0
-      @budget.year = 2021
-      @budget.save
-      @expense.save
-      expect(@expense.valid?).to be(false)
-      expect(@expense.errors.full_messages.include?("Budget for January, 2020 is not submitted yet!!")).to be(true)
-      expect(count).to eq(Expense.count)
-    end
-  end
+    context "#valid budget year and month not exist" do
+      it "should be created with valid budget year" do
+        count = 0
+        @budget.year = 2021
+        @budget.save
+        @expense.save
+        expect(@expense.valid?).to be(false)
+        expect(@expense.errors.full_messages.include?("Budget for January, 2020 is not submitted yet!!")).to be(true)
+        expect(count).to eq(Expense.count)
+      end
 
-  describe "Expense" do
-    it "should be created with valid budget month" do
-      count = 0
-      @budget.month = 8
-      @budget.save
-      @expense.save
-      expect(@expense.valid?).to be(false)
-      expect(@expense.errors.full_messages.include?("Budget for January, 2020 is not submitted yet!!")).to be(true)
-      expect(count).to eq(Expense.count)
+      it "should be created with valid budget month" do
+        count = 0
+        @budget.month = 8
+        @budget.save
+        @expense.save
+        expect(@expense.valid?).to be(false)
+        expect(@expense.errors.full_messages.include?("Budget for January, 2020 is not submitted yet!!")).to be(true)
+        expect(count).to eq(Expense.count)
+      end
     end
   end
 end
