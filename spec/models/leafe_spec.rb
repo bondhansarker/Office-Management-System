d4a2d6cc7b61a1b0b5786d6bad6fd3941c46fba9
Shariful Alam <shariful.alam85@gmail.com>
2020-03-24 19:13:28 +0600

    user spec and leafe spec updated

--- spec/models/leafe_spec.rb
+++ spec/models/leafe_spec.rb
@@ -4,12 +4,33 @@ RSpec.describe Leafe, type: :model do
   before(:each) do
     @user = FactoryBot.create(:user)
     @allocated_leafe = FactoryBot.create(:allocated_leafe, user: @user)
-    @leafe = FactoryBot.build(:leafe, user: @user)
   end
 
   describe "Leafe" do
+    it "should not be created without start date" do
+      @leafe = FactoryBot.build(:leafe, user: @user)
+      @leafe.start_date = nil
+      @leafe.save
+      expect(@leafe.valid?).to eq(false)
+    end
+
+    it "should not be created without end date" do
+      @leafe = FactoryBot.build(:leafe, user: @user)
+      @leafe.end_date = nil
+      @leafe.save
+      expect(@leafe.valid?).to eq(false)
+    end
+
+    it "should not be created without reason" do
+      @leafe = FactoryBot.build(:leafe, user: @user)
+      @leafe.reason = nil
+      @leafe.save
+      expect(@leafe.valid?).to eq(false)
+    end
+
     it "should be created with valid attribute" do
       count = 0
+      @leafe = FactoryBot.build(:leafe, user: @user)
       @leafe.save
       expect(@leafe.valid?).to eq(true)
       expect(count+1).to eq(Leafe.count)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,36 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
   describe "User" do
+    it "should not be created without email" do
+      @user.email = nil
+      @user.save
+      expect(@user.valid?).to eq(false)
+    end
+
+    it "should not be created without name" do
+      @user.name = nil
+      @user.save
+      expect(@user.valid?).to eq(false)
+    end
+
+    it "should not be created without phone" do
+      @user.phone = nil
+      @user.save
+      expect(@user.valid?).to eq(false)
+    end
+
+    it "should not be created without target amount" do
+      @user.target_amount = nil
+      @user.save
+      expect(@user.valid?).to eq(false)
+    end
+
+    it "should not be created without bonus percentage" do
+      @user.bonus_percentage = nil
+      @user.save
+      expect(@user.valid?).to eq(false)
+    end
+
     it "should be created with valid attribute" do
 
       count = 0
