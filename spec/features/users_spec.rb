eae743a46e4c743fd60161807623349d189aecab
bondhansarker <bondhansarker100@gmail.com>
2020-03-27 03:18:45 +0600

    rails helper modified feature testing folder created

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "Users", type: :feature do
+
+  context "sign in" do
+
+    scenario "should be successful" do
+
+    end
+
+    scenario "should fail" do
+
+    end
+
+  end
+end
