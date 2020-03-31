90e7e6f49aee4978ed896a379b109e7944bc2cca
bondhansarker <bondhansarker100@gmail.com>
2020-03-31 17:02:15 +0600

    expense system testing added

--- spec/features/expenses_spec.rb
+++ spec/features/expenses_spec.rb
@@ -2,44 +2,65 @@ require 'rails_helper'
 driver = Selenium::WebDriver.for :firefox
 
 RSpec.feature "Expense", type: :feature do
-  context "Expense" do
-    it "displays operations of the application" do
-      driver.get("http://localhost:3000")
 
+  before(:each) do
+    login_as_employee(driver)
+  end
+
+  context "Expense" do
+    it "should be successful" do
       driver.manage().window().maximize()
-      sleep(2)
-      driver.find_element(:css, "#user_email").send_keys("shariful.alam@rightcodes.org")
-      driver.find_element(:css, "#user_password").send_keys("111111")
-      sleep(2)
-      driver.find_element(css: "input[type='submit']").click
-      sleep(2)
+      sleep(1)
       driver.find_element(:css, "#expnese_menu").click
-      sleep(20)
+      sleep(1)
+      driver.find_element(:css, "#navbarResponsive > ul > li:nth-child(1) > div > div > div > a:nth-child(2)").click
+      sleep(1)
+      driver.find_element(:css, "#expnese_menu").click
+      sleep(1)
       driver.find_element(:css, "#navbarResponsive > ul > li:nth-child(1) > div > div > div > a:nth-child(1)").click
-      sleep(2)
+      sleep(1)
       driver.find_element(:css, "#expense_product_name").send_keys("Test expense")
       driver.find_element(:css, "#expense_category_id").click
-      driver.find_element(:css, "#expense_category_id").click
+      driver.find_element(:css, "#expense_category_id > option:nth-child(2)").click
+      sleep(1)
       driver.find_element(:css, "#expense_cost").send_keys("100")
       driver.find_element(:css, "#expense_details").send_keys("Running test")
       driver.find_element(:css, "#datepicker").send_keys("2020-03-21")
-      sleep(2)
+      sleep(1)
       driver.find_element(css: "input[type='submit']").click
-      sleep(2)
+      sleep(1)
       driver.find_element(css: ".fa-eye").click
-      sleep(2)
+      sleep(1)
       driver.find_element(css: ".btn-dark").click
-      sleep(2)
-      driver.find_element(css: ".fa-edit").click
-      sleep(2)
-      driver.find_element(:css, "#expense_product_name").send_keys(" Edited ")
-      sleep(2)
-      driver.find_element(css: "input[type='submit']").click
-      sleep(2)
+      sleep(1)
       driver.find_element(css: ".fa-trash").click
-      sleep(2)
+      sleep(1)
       driver.switch_to().alert().accept()
-      sleep(2)
+      sleep(1)
+      driver.find_element(:css, "#expnese_menu").click
+      sleep(1)
+      driver.find_element(:css, "#navbarResponsive > ul > li:nth-child(1) > div > div > div > a:nth-child(1)").click
+      sleep(1)
+      driver.find_element(:css, "#expense_product_name").send_keys("Test expense")
+      driver.find_element(:css, "#expense_category_id").click
+      driver.find_element(:css, "#expense_category_id > option:nth-child(2)").click
+      sleep(1)
+      driver.find_element(:css, "#expense_cost").send_keys("100")
+      driver.find_element(:css, "#expense_details").send_keys("Running test")
+      driver.find_element(:css, "#datepicker").send_keys("2020-03-21")
+      sleep(1)
+      driver.find_element(css: "input[type='submit']").click
+      sleep(1)
+      driver.find_element(css: ".fa-eye").click
+      sleep(1)
+      driver.find_element(css: ".btn-dark").click
+      sleep(1)
+      driver.find_element(css: ".fa-edit").click
+      sleep(1)
+      driver.find_element(:css, "#expense_product_name").send_keys(" Edited ")
+      sleep(1)
+      driver.find_element(css: "input[type='submit']").click
+      sleep(1)
       driver.quit
     end
   end
